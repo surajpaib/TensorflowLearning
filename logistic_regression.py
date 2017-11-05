b3665eb63454110a314d2781ed6b679b4b8c10a9
Suraj Pai <surajballambat@gmail.com>
2017-11-05 15:13:35 +0530

    Add logistic regressor class

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,57 +4,64 @@ from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
 
-# Load the dataset
-iris = load_iris()
-iris_X, iris_y = iris.data[:-1,:], iris.target[:-1]
-iris_y= pd.get_dummies(iris_y).values
-trainX, testX, trainY, testY = train_test_split(iris_X, iris_y, test_size=0.33, random_state=42)
-print trainX[0], trainY[0]
 
-# Get shape of input and output variables
-numFeatures = trainX.shape[1]
-numLabels = trainY.shape[1]
+class LogisticRegression:
+    def __init__(self, lr=0.004):
+        self.lr = lr
+
+    def _construct_graph(self):
+        self.x = tf.placeholder(tf.float32, [None, self.numFeatures])
+        self.y = tf.placeholder(tf.float32, [None, self.numLabels])
+        self.weights = tf.Variable(np.random.rand(self.numFeatures, self.numLabels).astype(np.float32))
+        self.bias = tf.Variable(np.random.rand(self.numLabels).astype(np.float32))
+        Wx = tf.matmul(self.x, self.weights)
+        Wxb = tf.add(Wx, self.bias)
+        Activation = tf.nn.sigmoid(Wxb)
+        learningRate = tf.train.exponential_decay(learning_rate=self.lr,
+                                                  global_step=1,
+                                                  decay_steps=trainX.shape[0],
+                                                  decay_rate=0.95,
+                                                  staircase=True)
+
+        self.mse = tf.nn.l2_loss(self.y - Activation, name='loss')
+        self.optimizer = tf.train.GradientDescentOptimizer(learningRate)
+        self.loss = self.optimizer.minimize(self.mse)
+        self.init = tf.global_variables_initializer()
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Activation, 1), tf.argmax(self.y, 1)), "float"))
+
+    def fit(self, X, Y):
+        self.numFeatures = X.shape[1]
+        self.numLabels= Y.shape[1]
+        self._construct_graph()
+
+        iterations = 10000
+        with tf.Session() as sess:
+            sess.run(self.init)
+            for i in range(iterations):
+                sess.run(self.loss, feed_dict={self.x: X, self.y: Y})
+                if i % 50 == 0:
+                    accuracy_val = sess.run(self.accuracy, feed_dict={self.x: X, self.y: Y})
+                    mse_error = sess.run(self.mse, feed_dict={self.x: X, self.y: Y})
+                    print "Accuracy: " + str(accuracy_val * 100) + " Loss: " + str(mse_error)
+
+    def predict(self, X, Y):
+        with tf.Session() as sess:
+            sess.run(self.init)
+            accuracy_val = sess.run(self.accuracy, feed_dict={self.x: X, self.y: Y})
+            print accuracy_val
+
+
+if __name__ == "__main__":
+    iris = load_iris()
+    iris_X, iris_y = iris.data[:-1,:], iris.target[:-1]
+    iris_y= pd.get_dummies(iris_y).values
+    trainX, testX, trainY, testY = train_test_split(iris_X, iris_y, test_size=0.33, random_state=42)
+    print trainX[0], trainY[0]
+
+    # Get shape of input and output variables
+    logregressor = LogisticRegression()
+    logregressor.fit(trainX, trainY)
 
 # Set placeholders training examples
-x = tf.placeholder(tf.float32, [None, numFeatures])
-y = tf.placeholder(tf.float32, [None, numLabels])
-
-# Set weight and bias variables
-# W = tf.Variable(tf.zeros([4, 3]))
-# b = tf.Variable(tf.zeros([3]))
-
-weights = tf.Variable(np.random.rand(4, 3).astype(np.float32))
-bias = tf.Variable(np.random.rand(3).astype(np.float32))
-print weights, bias
-
-Wx = tf.matmul(x, weights)
-Wxb = tf.add(Wx, bias)
-Activation = tf.nn.sigmoid(Wxb)
-
-learningRate = tf.train.exponential_decay(learning_rate=0.004,
-                                          global_step= 1,
-                                          decay_steps=trainX.shape[0],
-                                          decay_rate= 0.95,
-                                          staircase=True)
-
-mse = tf.nn.l2_loss(y - Activation, name='loss')
-optimizer = tf.train.GradientDescentOptimizer(learningRate)
-loss = optimizer.minimize(mse)
-init = tf.global_variables_initializer()
-
-accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Activation, 1), tf.argmax(y, 1)), "float"))
-iterations = 10000
-with tf.Session() as sess:
-    sess.run(init)
-    for i in range(iterations):
-        loss_rate = sess.run(loss, feed_dict={x: trainX, y: trainY})
-        if i % 50 == 0:
-            accuracy_val = sess.run(accuracy, feed_dict={x: trainX, y: trainY})
-            mse_error = sess.run(mse, feed_dict={x: trainX, y: trainY})
-            print "Accuracy: " + str(accuracy_val * 100) + " Loss: " + str(mse_error)
-    test_accuracy = sess.run(accuracy, feed_dict={x: testX, y: testY})
-    print "Test Set Accuracy:" + str(test_accuracy * 100)
-
-
 
 
